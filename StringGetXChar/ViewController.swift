c086ddad7803359c993ba0a42baaf94b00d5cc0b
Andrew Seeley <awseeley@gmail.com>
2017-01-29 21:35:17 +1100

    Updated for Swift 3
    Updated for Swift 3

--- StringGetXChar/ViewController.swift
+++ StringGetXChar/ViewController.swift
@@ -13,6 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let str = "Seemu Apps"
+        
+        // 1
+        // Get a character at X position (index)
+        let index = str.characters.index(str.startIndex, offsetBy: 4)
+        let startChar = str[index] // returns Character 'u'
+        print(startChar)
+        
+        // 2
+        // Get a character at X position (index) starting from the end of the string
+        let endIndex = str.characters.index(str.endIndex, offsetBy: -2) // Goes to the end of the string and back to characters
+        let endChar = str[endIndex] // returns Character "p"
+        print(endChar)
+        
+        // 3
+        // Get the substring, starting from index and ending at endIndex
+        let subString = str[(index ..< endIndex)]
+        print(subString) // returns "u Ap"
+        
+        // 4 & 5
+        // Get the substring starting from Index until the End of the String
+        let indexToEnd = str.substring(from: index)
+        // Get the substring starting frmo the Start of the String to the Index
+        let startToIndex = str.substring(to: index)
+        print(indexToEnd) // returns String "u Apps"
+        print(startToIndex) // returns String "Seem"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
